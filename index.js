aa09d3a76c5d5a916f312672ae1cfcd2e83e9a71
keerti <sharmakeerti2302@gmail.com>
2024-05-03 21:42:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config() //require is used to import the package into the code
+const express = require('express')
+const app = express()
+const port = 3001
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send('keerti.com')
+})
+
+app.get('/login',(req,res)=>{
+    res.send('<h1> Please login at chai aur code</h1>')
+})
+
+app.get('/youtube',(req,res)=>{
+    res.send("Chai aur Code")
+}
+)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
